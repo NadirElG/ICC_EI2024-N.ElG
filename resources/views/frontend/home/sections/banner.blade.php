921b0d6c7be12235798af75622f01c90591567a6
NadirElG <107560919+NadirElG@users.noreply.github.com>
2024-11-08 18:24:58 +0100

    fix(HomeSection): ReOrganize

---
+++ resources/views/frontend/home/sections/banner.blade.php
@@ -0,0 +1,25 @@
+    <!--============================
+        PLANS BANNER START
+    ==============================-->
+    <section id="wsus__monthly_top" class="wsus__monthly_top_2">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12 col-lg-12">
+                    <div class="wsus__monthly_top_banner">
+                        <div class="wsus__monthly_top_banner_img">
+                            <img src="{{ asset('frontend/images/about_2.jpg') }}" alt="Wallet" class="img-fluid w-100">
+                            <span></span>
+                        </div>
+                        <div class="wsus__monthly_top_banner_text">
+                            <h3>Add Funds with Our <span>Exclusive Plans</span></h3>
+                            <h6>Top-up Now</h6>
+                            <a class="shop_btn" href="{{ route('plans') }}">View Plans</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--============================
+        PLANS BANNER END
+    ==============================-->
